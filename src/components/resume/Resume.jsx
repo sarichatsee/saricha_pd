fed588de2ea144b39acb6bd611d5f9a4f46eeba8
fann <22034013@myrp.edu.sg>
2024-11-06 22:31:59 +0800

    added marquee(s)

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -2,12 +2,34 @@ import React from "react";
 import "./resume.css";
 import Data from "./Data";
 import Card from "./Card";
+import "./wrapper.css";
 
 const Resume = () => {
     return (
         <section className="resume container section" id="resume">
             <h2 className="section__title">Experience</h2>
-
+            <div className="wrapper__container">
+                <div class="wrapper">
+                    <div class="itemLeft item1"></div>
+                    <div class="itemLeft item2"></div>
+                    <div class="itemLeft item3"></div>
+                    <div class="itemLeft item4"></div>
+                    <div class="itemLeft item5"></div>
+                    <div class="itemLeft item6"></div>
+                    <div class="itemLeft item7"></div>
+                    <div class="itemLeft item8"></div>
+                </div>
+                <div class="wrapper">
+                    <div class="itemRight item1"></div>
+                    <div class="itemRight item2"></div>
+                    <div class="itemRight item3"></div>
+                    <div class="itemRight item4"></div>
+                    <div class="itemRight item5"></div>
+                    <div class="itemRight item6"></div>
+                    <div class="itemRight item7"></div>
+                    <div class="itemRight item8"></div>
+                </div>
+            </div>
             <div className="resume__container grid">
                 <div className="timeline grid">
                     {Data.map((val, id) => {
