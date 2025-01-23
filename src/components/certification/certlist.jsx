6e6234aaca85f910d95aa1bccdaa0cf7e818cea2
fann <22034013@myrp.edu.sg>
2025-01-23 19:26:32 +0800

    updated css styling for certificates - might try to make it more responsive

--- src/components/certification/certlist.jsx
+++ src/components/certification/certlist.jsx
@@ -1,9 +1,19 @@
 import Certificate1 from "../../assets/cert-imgs/2023-Y1S1-SOI-DDDD.png";
 import Certificate2 from "../../assets/cert-imgs/2024-Y2S1-SOI-DDDD.png";
 import Certificate3 from "../../assets/cert-imgs/DSTA-Brainhack-codeEXP-participation-finalist.png";
-import Certificate4 from "../../assets/cert-imgs/17176_5_6560766_1730276119_AWS-Skill Builder-Course-Completion-Certificate-1.png";
-import Certificate5 from "../../assets/cert-imgs/PCEP-Certified Entry-Level-Python-Programmer-1.png";
-// src/assets/Certficiate/2023-Y1S1-SOI-DDDD.png
+import Certificate4 from "../../assets/cert-imgs/17176_5_6560766_1730276119_AWS-Skill-Builder-Course-Completion-Certificate-1.png";
+import Certificate5 from "../../assets/cert-imgs/PCEP-Certified-Entry-Level-Python-Programmer-1.png";
+import Certificate6 from "../../assets/cert-imgs/coursera-google-ux-design.png";
+import Certificate7 from "../../assets/cert-imgs/1-CertificateOfCompletion_BuildingaWebsitewithNode.jsandExpress.js-1.png";
+import Certificate8 from "../../assets/cert-imgs/2-CertificateOfCompletion_CreateaQuickClean_andCheapWebsitewithBootstrapTemplates-1.png";
+import Certificate9 from "../../assets/cert-imgs/3-CertificateOfCompletion_HandsOnIntroductionReact-1.png";
+import Certificate10 from "../../assets/cert-imgs/4-CertificateOfCompletion_Illustrator2023QuickStart-1.png";
+import Certificate11 from "../../assets/cert-imgs/5-CertificateOfCompletion_IntroductiontoWebDesignandDevelopment-1.png";
+import Certificate12 from "../../assets/cert-imgs/6-CertificateOfCompletion_LearningNode.js-1.png";
+import Certificate13 from "../../assets/cert-imgs/7-CertificateOfCompletion_ProgrammingFoundationsSoftwareTestingQA-1.png";
+import Certificate14 from "../../assets/cert-imgs/8-CertificateOfCompletion_SoftwareTestingFoundationsTestPlanning-1.png";
+import Certificate15 from "../../assets/cert-imgs/9-designing-accessible-components-in-figma.png";
+
 
 const CertData = [
     {
@@ -35,6 +45,66 @@ const CertData = [
         image: Certificate5,
         title: "PCEP - Certified Entry-Level Python Programmer",
         category: "2024",
+    },
+    {
+        id: 6,
+        image: Certificate6,
+        title: "Google UX Design",
+        category: "2024",
+    },
+    {
+        id: 7,
+        image: Certificate7,
+        title: "Building a Website with Node.js and Express.js",
+        category: "2024",
+    },
+    {
+        id: 8,
+        image: Certificate8,
+        title: "Create a Quick, Clean and Cheap Website with Bootstrap Templates",
+        category: "2024",
+    },
+    {
+        id: 9,
+        image: Certificate9,
+        title: "Hands-On Introduction: React",
+        category: "2024",
+    },
+    {
+        id: 10,
+        image: Certificate10,
+        title: "Illustrator 2023 Quick Start",
+        category: "2024",
+    },
+    {
+        id: 11,
+        image: Certificate11,
+        title: "Introduction to Web Design and Development",
+        category: "2024",
+    },
+    {
+        id: 12,
+        image: Certificate12,
+        title: "Learning Node.js",
+        category: "2024",
+    },
+    {
+        id: 13,
+        image: Certificate13,
+        title: "Programming Foundations: Software Testing/QA",
+        category: "2024",
+    },
+    {
+        id: 14,
+        image: Certificate14,
+        title: "Software Testing Foundations: Test Planning",
+        category: "2024",
+    },
+    {
+        id: 15,
+        image: Certificate15,
+        title: "Designing Accessible Components in Figma",
+        category: "2025",
     },
 ];
 
